fa72ad70c8354df81e0ac482a0a1b99d41390ade
JrzenonDev <jrobertoonb@gmail.com>
2023-10-26 13:36:29 -0300

    feat: create constants api

---
+++ src/constants/api.js
@@ -0,0 +1,14 @@
+const API_URL = "http://localhost:3000/";
+
+const BRASIL_API_URL = "https://brasilapi.com.br/api/cptec/v1/clima/";
+
+const BRASIL_API_CITY_URL = "previsao";
+
+const BRASIL_API_AIRPORT_URL = "aeroporto";
+
+module.exports = {
+  API_URL,
+  BRASIL_API_URL,
+  BRASIL_API_CITY_URL,
+  BRASIL_API_AIRPORT_URL,
+};
