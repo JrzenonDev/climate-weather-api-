0ed129cf7c286483a73b13be7094be320f4726d9
JrzenonDev <jrobertoonb@gmail.com>
2023-10-27 03:03:11 -0300

    feat: add commits JSDoc to generate swagger documentation of endpoints

--- src/routes/airport.js
+++ src/routes/airport.js
@@ -1,3 +1,32 @@
+/**
+ * @swagger
+ * /airport/{airportCode}:
+ *   get:
+ *     summary: Get weather information for a specific airport.
+ *     parameters:
+ *       - in: path
+ *         name: airportCode
+ *         required: true
+ *         description: The IATA code of the airport.
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successful response with weather information.
+ *         content:
+ *           application/json:
+ *             example: '{"umidade": 93, "visibilidade": ">10000", "codigo_icao": "SBGR", "pressao_atmosferica": 1014, "vento": 4, "direcao_vento": 140, "condicao": "ps", "condicao_desc": "Predomínio de Sol", "temp": 18, "atualizado_em": "2023-10-26T22:00:00.827Z"}'
+ *       404:
+ *         description: Airport not found.
+ *         content:
+ *           application/json:
+ *             example: '{"error": "Airport not found."}'
+ *       500:
+ *         description: Internal Server Error.
+ *         content:
+ *           application/json:
+ *             example: '{"error": "Error fetching weather data for the airport."}'
+ */
+
 const express = require("express");
 
 const { getAirportWeather } = require("../controllers/airportController");
--- src/routes/city.js
+++ src/routes/city.js
@@ -1,3 +1,32 @@
+/**
+ * @swagger
+ * /city/{cityCode}:
+ *   get:
+ *     summary: Get weather information for a specific city.
+ *     parameters:
+ *       - in: path
+ *         name: cityCode
+ *         required: true
+ *         description: The city code or identifier.
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successful response with weather information.
+ *         content:
+ *           application/json:
+ *             example: '{"cidade": "Brejo Alegre", "estado": "SP", "atualizado_em": "2023-10-26", "clima": [{"data": "2023-10-27", "condicao": "c", "condicao_desc": "Chuva", "min": 23, "max": 32, "indice_uv": 13}]}'
+ *       404:
+ *         description: City not found.
+ *         content:
+ *           application/json:
+ *             example: '{"error": "City not found."}'
+ *       500:
+ *         description: Internal Server Error.
+ *         content:
+ *           application/json:
+ *             example: '{"error": "Error fetching weather data for the city."}'
+ */
+
 const express = require("express");
 
 const { getCityWeather } = require("../controllers/cityController");
